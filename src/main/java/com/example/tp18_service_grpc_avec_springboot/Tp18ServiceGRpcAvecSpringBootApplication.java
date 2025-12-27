69897940e31a497f5050784bc91227536add61f1
abdelboulgha <118727738+abdelboulgha@users.noreply.github.com>
2025-12-27 11:30:58 +0100

    my first commit

---
+++ src/main/java/com/example/tp18_service_grpc_avec_springboot/Tp18ServiceGRpcAvecSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp18_service_grpc_avec_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp18ServiceGRpcAvecSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp18ServiceGRpcAvecSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp18_service_grpc_avec_springboot/Tp18ServiceGRpcAvecSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp18_service_grpc_avec_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp18ServiceGRpcAvecSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
